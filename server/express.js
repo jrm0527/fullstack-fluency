cfc50cbb588c49098ae167e1bd23c029db690283
jrm0527 <geeziegames@gmail.com>
2023-04-12 12:10:36 -0400

    added update and delete

--- server/express.js
+++ server/express.js
@@ -26,12 +26,77 @@ app.get("/api/tasks", async (req, res, next) => {
 
 app.get("/api/tasks/:taskId", async (req, res, next) => {
   const taskId = req.params.taskId;
+  const query = {
+    text: "SELECT * FROM task WHERE id = $1",
+    values: [taskId],
+  };
   try {
     const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-    const { rows } = await pool.query(
-      `SELECT * FROM task WHERE id = ${taskId}`
-    );
+    const { rows } = await pool.query(query);
     res.send(rows);
+    pool.end();
+  } catch (error) {
+    next({ status: 400, message: error.message });
+  }
+});
+
+app.post("/api/tasks/", async (req, res, next) => {
+  const taskName = req.body.task;
+  const completed = false;
+
+  const query = {
+    text: "INSERT INTO task (task, complete) VALUES ($1, $2) RETURNING *",
+    values: [taskName, completed],
+  };
+
+  try {
+    const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+    const { rows } = await pool.query(query);
+    res.send(rows);
+    pool.end();
+  } catch (error) {
+    next({ status: 400, message: error.message });
+  }
+});
+
+app.delete("/api/tasks/:taskId", async (req, res, next) => {
+  const taskId = req.params.taskId;
+
+  const query = {
+    text: "DELETE FROM task WHERE id = $1 RETURNING *",
+    values: [taskId],
+  };
+  try {
+    const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+    const { rows } = await pool.query(query);
+    if (rows.length === 0) {
+      next({ status: 400, message: "Bad Request" });
+    } else {
+      res.send(rows);
+    }
+    pool.end();
+  } catch (error) {
+    next({ status: 400, message: error.message });
+  }
+});
+
+app.patch("/api/tasks/:taskId", async (req, res, next) => {
+  const taskId = req.params.taskId;
+  const taskName = req.body.task;
+  const completed = req.body.complete;
+
+  const query = {
+    text: "UPDATE task SET task = COALESCE($2, task), complete = COALESCE($3, complete) WHERE id = $1 RETURNING *",
+    values: [taskId, taskName || null, completed || null],
+  };
+  try {
+    const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+    const { rows } = await pool.query(query);
+    if (rows.length === 0) {
+      next({ status: 400, message: "Bad Request" });
+    } else {
+      res.send(rows);
+    }
     pool.end();
   } catch (error) {
     next({ status: 400, message: error.message });
